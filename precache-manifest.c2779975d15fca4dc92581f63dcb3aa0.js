ea9c0292e43df566beb327ba490952319bd5e590
baiyjmatheus <baiyj.matheus@gmail.com>
2020-04-12 15:14:46 -0300

    Updates

--- precache-manifest.c2779975d15fca4dc92581f63dcb3aa0.js
+++ precache-manifest.c2779975d15fca4dc92581f63dcb3aa0.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7910447d0a22599313559dea98436706",
+    "revision": "1785dbe8a6bc49a066abf92db34388d3",
     "url": "/quiz/index.html"
   },
   {
-    "revision": "00b301377e0fef636c99",
-    "url": "/quiz/static/css/main.1add707b.chunk.css"
+    "revision": "342c89379699869c7e7d",
+    "url": "/quiz/static/css/main.38c6b19b.chunk.css"
   },
   {
     "revision": "252eb17c3c018093441e",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/quiz/static/js/2.c5e26969.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "00b301377e0fef636c99",
-    "url": "/quiz/static/js/main.57e531fc.chunk.js"
+    "revision": "342c89379699869c7e7d",
+    "url": "/quiz/static/js/main.2884c213.chunk.js"
   },
   {
     "revision": "85641e1b79385260e69a",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/quiz/precache-manifest.f774d54e038399b67dad5d16ddc24ea5.js"
+  "/quiz/precache-manifest.c2779975d15fca4dc92581f63dcb3aa0.js"
 );
 
 self.addEventListener('message', (event) => {
