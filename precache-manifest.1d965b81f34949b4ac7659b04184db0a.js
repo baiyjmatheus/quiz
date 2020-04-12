ae73729c681bd9e02bb8d835b6d8d11646eb04bf
baiyjmatheus <baiyj.matheus@gmail.com>
2020-04-11 23:06:16 -0300

    Updates

---
+++ precache-manifest.1d965b81f34949b4ac7659b04184db0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d09a00678e1516e1c2960d59383b3eb6",
+    "url": "/quiz/index.html"
+  },
+  {
+    "revision": "e1c28e6457e311b7e36e",
+    "url": "/quiz/static/css/main.1add707b.chunk.css"
+  },
+  {
+    "revision": "252eb17c3c018093441e",
+    "url": "/quiz/static/js/2.c5e26969.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/quiz/static/js/2.c5e26969.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1c28e6457e311b7e36e",
+    "url": "/quiz/static/js/main.c07880dc.chunk.js"
+  },
+  {
+    "revision": "85641e1b79385260e69a",
+    "url": "/quiz/static/js/runtime-main.e32e060b.js"
+  }
+]);
